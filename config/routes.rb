d01fe458e8adae91f5220c7232929d22e96fd295
Albert Lee <yoonpoon@gmail.com>
2017-08-14 13:15:18 +0200

    done but no styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/answer', to: 'questions#answer'
+  # get '/answer/query' => 'questions#answer'
+  get '/ask', to: 'questions#ask'
 end
